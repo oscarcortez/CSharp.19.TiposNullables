6787085472f876806264c3ece1731a1e7349f03b
oscarcortez <oscarkortez@gmail.com>
2021-03-31 20:02:56 -0400

    Add project files.

---
+++ ConsoleApp4TiposNullables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp4TiposNullables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int? enteroNulo = null;
+            enteroNulo = 123;
+
+            Console.WriteLine($"Entero Nulo: {enteroNulo} {enteroNulo.Value}");
+            int enteroNormal = 0;
+            if(enteroNulo.HasValue)
+            {
+                // enteroNormal = enteroNulo;
+                Console.WriteLine($"SI tiene value");
+                enteroNormal = enteroNulo.Value;
+            }
+
+        }
+    }
+}
